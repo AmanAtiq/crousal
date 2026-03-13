affe0f7d5cc6a89bf5d998e42506272982a1ad14
aman malik <amanmalik@192.168.1.2>
2026-03-13 05:25:07 +0500

    fix17

--- client/pages/Delicias.tsx
+++ client/pages/Delicias.tsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/Navbar";
+ 
 import { CustomerTestimonialsSection } from "@/components/delicias/sections/CustomerTestimonialsSection";
 import { FooterSection } from "@/components/delicias/sections/FooterSection";
 import { HeroIntroSection } from "@/components/delicias/sections/HeroIntroSection";
@@ -6,11 +6,11 @@ import { ProductGallerySection } from "@/components/delicias/sections/ProductGal
 import { StoreLocationsSection } from "@/components/delicias/sections/StoreLocationsSection";
 
 
+import { NavigationHeaderSection } from "@/components/delicias/sections/NavigationHeaderSection";
 export default function Delicias() {
-
   return (
     <div className="relative w-full overflow-x-hidden bg-[linear-gradient(0deg,rgba(254,250,247,1)_0%,rgba(254,250,247,1)_100%),linear-gradient(0deg,rgba(255,255,255,1)_0%,rgba(255,255,255,1)_100%)]">
-      <Navbar />
+     <NavigationHeaderSection />
       <HeroIntroSection />
       <StoreLocationsSection />
       <ProductGallerySection />
